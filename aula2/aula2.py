3f264c6c830b2e7cb2532d2ab39f0b3e8afe8b07
wladiveras <wladinart@gmail.com>
2021-10-03 20:52:28 -0300

    Continuação da aula

--- aula2/aula2.py
+++ aula2/aula2.py
@@ -1,3 +1,13 @@
 
-print(r'test da sua Mc\'s Donald\'s ')
-print('xd')
+# ! Instância as variaveis
+__name = 'wladi veras'
+__age  = 24
+
+# ! Mostra a idade + tipo de objeto.
+print (__name, type(__name))
+
+# ! Mostra a idade + tipo de objeto.
+print (__age, type(__age))
+
+# ! Verifica se a idade é maior que 18 + tipo de objetivo.
+print (__age >= 18, type(__age >= 18))
